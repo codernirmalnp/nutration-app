2906547cba23e2bf082ba9f11b41bfd927ef9659
Codernirmal <61540469+codernirmalnp@users.noreply.github.com>
2021-02-13 19:31:07 +0545

    Update itemCard.js

--- client/src/components/itemCard.js
+++ client/src/components/itemCard.js
@@ -7,7 +7,7 @@ const ItemCard = ({ item }) => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <Link to={`product/${item._id}`}>
-      <img src={`https://codernirmalapi.herokuapp.com/uploads/${item.image}`} alt="" className="w-300 h-300 overflow-hidden"/>
+      <img src={`https://nutrationapp.herokuapp.com//uploads/${item.image}`} alt="" className="w-300 h-300 overflow-hidden"/>
       </Link>
       
       <div className="px-6 py-4">
