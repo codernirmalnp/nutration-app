467325fabd2f62237f61fba76f6bd33f62872572
Codernirmal <61540469+codernirmalnp@users.noreply.github.com>
2021-02-13 20:08:51 +0545

    Update server.js

--- server.js
+++ server.js
@@ -309,7 +309,7 @@ async function connect() {
   } catch (err) {
     console.log('Mongoose error', err);
   }
-  app.listen(process.env.PORT);
+  app.listen(process.env.PORT || 5000);
   console.log(`API listening on localhost:${process.env.PORT}`);
 }
 
